4f6006df6a6d2c64d4caffd574fec963330fc0e9
Jonah Turnquist <jonah@getparampara.com>
2020-02-25 10:20:28 +1100

    Removed broken scroll behaviour

--- frontend/src/components/chat-player/components/action-types/ChooseResponse.tsx
+++ frontend/src/components/chat-player/components/action-types/ChooseResponse.tsx
@@ -39,12 +39,6 @@ type Props = {
 }
 
 class ChooseResponse extends React.Component<Props> {
-  containerRef: React.RefObject<HTMLDivElement> = React.createRef()
-
-  componentDidMount() {
-    this.containerRef.current!.scrollIntoView({ behavior: 'smooth' })
-  }
-
   handleClick = (choice: number) => (event: any) => {
     event.preventDefault()
 
@@ -61,7 +55,7 @@ class ChooseResponse extends React.Component<Props> {
 
   render() {
     return (
-      <Wrapper ref={this.containerRef}>
+      <Wrapper>
         {this.props.action.responses.map((response, i) =>
           <ResponseButton key={i} variant="link" onClick={this.handleClick(i)}>{response.message}</ResponseButton>
         )}
--- frontend/src/components/chat-player/components/action-types/TextInput.tsx
+++ frontend/src/components/chat-player/components/action-types/TextInput.tsx
@@ -47,11 +47,6 @@ class TextInput extends React.Component<Props, State> {
   state: State = {
     content: ''
   }
-  containerRef: React.RefObject<HTMLDivElement> = React.createRef()
-
-  componentDidMount() {
-    this.containerRef.current!.scrollIntoView({ behavior: 'smooth' })
-  }
 
   updateContent = (event: any) => {
     event.preventDefault()
@@ -67,7 +62,7 @@ class TextInput extends React.Component<Props, State> {
 
   render() {
     return (
-      <Wrapper ref={this.containerRef}>
+      <Wrapper>
         <Form inline={true} onSubmit={this.handleSubmit}>
           <MessageInput placeholder={this.props.placeholder} onChange={this.updateContent} autoFocus />
           <GoButton variant="primary" type="submit">Go</GoButton>
