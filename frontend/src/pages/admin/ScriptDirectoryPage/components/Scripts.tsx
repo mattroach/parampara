774e0b85aed4d46dbdf8b7cb2331656c1e54420c
Jonah Turnquist <jonah@getparampara.com>
2020-04-01 14:21:36 +1100

    Added upgrade note to home page

--- frontend/src/pages/admin/ScriptDirectoryPage/components/Scripts.tsx
+++ frontend/src/pages/admin/ScriptDirectoryPage/components/Scripts.tsx
@@ -37,6 +37,7 @@ const Scripts: React.FunctionComponent<Props> = ({ adminId }) => {
   if (hasScripts) {
     return (
       <>
+        <UpgradeNote />
         <AddNewButton>
           <CreateScriptButton adminId={adminId} variant="secondary" />
         </AddNewButton>
@@ -59,22 +60,26 @@ const Scripts: React.FunctionComponent<Props> = ({ adminId }) => {
           for a quick video tutorial and a step by step guide. You can always come back to
           this later in the menu at the top right of this page.
         </p>
-        <p>
-          Get even more out of your Parampara account. We can help with expert
-          copywriting, advanced analytics and on-call support. Head to our{' '}
-          <a
-            href="https://getparampara.com/pricing.html?r=inapp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            pricing page
-          </a>{' '}
-          for details.
-        </p>
+        <UpgradeNote />
         <CreateScriptButton adminId={adminId} />
       </EmptyState>
     )
   }
 }
+
+const UpgradeNote = () => (
+  <p>
+    Get even more out of your Parampara account. We can help with expert copywriting,
+    advanced analytics and on-call support. Head to our{' '}
+    <a
+      href="https://getparampara.com/pricing.html?r=inapp"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      pricing page
+    </a>{' '}
+    for details.
+  </p>
+)
 
 export default Scripts
