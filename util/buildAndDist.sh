f87bb6b04b508fdcfdaeee5b0f1ee97bf2079d12
Jonah Turnquist <jonah@getparampara.com>
2020-07-03 16:11:42 +1000

    optimized build script to run npm installs

--- util/buildAndDist.sh
+++ util/buildAndDist.sh
@@ -3,10 +3,12 @@ NC='\033[0m' # No Color
 
 printf "\n\n${RED}Building frontend${NC}\n"
 cd frontend
+npm install
 npm run-script build
 
 cd ..
 printf "\n\n${RED}Building backend${NC}\n"
+npm install
 npm run-script build
 printf "\n\n${RED}Creating dist file${NC}\n"
 npm run-script dist
